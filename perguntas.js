4b8c23df146da780460bf37b2dad2d004864239f
rafaellicalheiro <148975438+rafaellicalheiro@users.noreply.github.com>
2024-09-27 17:46:19 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Sonhos Caninos',
+    'Cachorros podem sonhar?',
+    'Sim, sonham e provavelmente com atividades diarias, como brincar e correr.'
+)
+
+criaCartao(
+    'Amassar Pãozinho',
+    'Por que gatos "amassam pãozinho" com as patinhas?',
+    'É um comportamento de conforto, herdado da fase de filhote, quando massageavam a mãe para o leite.'
+)
+
+criaCartao(
+    'Linguagem Canina',
+    'Os cães realmente entendem o que os humanos dizem ou só a intonação?',
+    'Eles entendem algumas palavras, mas o tom de voz é o mais imporante para a interpretação.'
+)
+
+criaCartao(
+    'Senso de tempo',
+    'Como os gatos sabem o horario exato para pedirem comida?',
+    'Eles têm um senso de tempo apurado, baseado na rotina dos donos e no ciclo de luz natural.'
+)
+
+criaCartao(
+    'Cabeça Inclinada',
+    'Por que os cães inclinam a cabeça quando escutam algo estranho?',
+    'Para ajustar as orelhas e ouvir melhor, ou para captar melhor as expressoões faciais.'
+)
+
+criaCartao(
+    'Curiosidade pela Água',
+    'Se os gatos detestam água, por que adoram torneiras pingando?',
+    'Eles são curiosos e se atraem pelo som e movimento da água, mesmo sem gostar de se molhar.'
+)
